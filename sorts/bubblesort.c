5990bd8cc2ff0f9bc32355adad981a69b929de06
Ravindu Herath <122708614+hercul3poirot@users.noreply.github.com>
2023-01-17 23:44:18 +1100

    Added the first sort

---
+++ sorts/bubblesort.c
@@ -0,0 +1,34 @@
+//This function performs a bubble sort, a sorting algorithm which works by repeatedly swapping the adjacent elements if they are in the wrong order.
+#include <stdio.h> //Required for the example, not required for the sort.
+
+void bubbleSort(int arr[], int length){
+    int start_position = 0, end_position = length, temp;
+    for (int i = 1; i < length; i++) {
+        while (start_position != end_position){
+            if (arr[start_position] > arr[start_position + 1]) {
+                temp = arr[start_position + 1];
+                arr[start_position + 1] = arr[start_position];
+                arr[start_position] = temp; 
+            }
+            start_position++;    
+        } 
+        end_position--;
+        start_position = 0;
+    }
+}
+
+//Example usage of the function above: 
+void main(){
+    int arr[] = {3,4,2}, length = sizeof(arr)/sizeof(arr[0]); 
+    /*Works mostly with arrays of any size, but for some reason the length variable trips out the results (tried on different compilers)
+    Best off explicitly defining length*/
+    printf("Original numbers: ");
+    for (int i = 0; i < length; i++){
+        printf("%i ", arr[i]);
+    }
+    bubbleSort(arr, length);
+    printf("\nSorted numbers: ");
+    for (int i = 0; i < length; i++){
+        printf("%i ", arr[i]);
+    }
+}
