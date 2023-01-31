565ed5aa300cefb1d3b473fc50447feaef162299
Ravindu Herath <122708614+hercul3poirot@users.noreply.github.com>
2023-01-31 20:39:06 +1100

    Update exponential.c
    Was not functioning with power to the zero

--- functions/exponential.c
+++ functions/exponential.c
@@ -1,6 +1,9 @@
 //This function returns a number exponentially multiplied, with the number and exponent determined by the user.
 int toThePowerOf(int number, int exponent) {
-    if (exponent == 1) {
+    if (exponent == 0) {
+        return 1;
+    }
+    else if (exponent == 1) {
         return number;
     } else {
         return number * toThePowerOf(number, exponent - 1);
